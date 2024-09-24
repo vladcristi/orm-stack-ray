20cd1faea023cc2d6145e37fbeff48cc2dedca33
vladcristi <cristian.vlad@oracle.com>
2024-09-24 22:23:51 +0300

    changed rayserverhandle to deploymenthandle

--- text_ml.py
+++ text_ml.py
@@ -35,7 +35,7 @@ class Translator:
 
 @serve.deployment
 class Summarizer:
-    def __init__(self, translator: RayServeHandle):
+    def __init__(self, translator: DeploymentHandle):
         # Load model
         self.model = pipeline("summarization", model="t5-small")
         self.translator = translator
